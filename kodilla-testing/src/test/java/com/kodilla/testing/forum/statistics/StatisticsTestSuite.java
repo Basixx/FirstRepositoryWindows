a3e3e5715c16d6211dd4fbce0ac7a901aab1c543
Barbara Rutkowska <barbara_rutkowska.22@wp.pl>
2021-10-07 10:10:44 +0200

    6.7. poprawione :)

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -1,9 +1,6 @@
 package com.kodilla.testing.forum.statistics;
 
-import com.kodilla.testing.forum.statistics.CountStatistics;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -11,7 +8,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
@@ -21,176 +17,194 @@ public class StatisticsTestSuite {
     @Mock
     private Statistics statisticsMock;
 
+    private static int testCounter = 0;
+
+    @BeforeAll
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterAll
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @BeforeEach
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
     @DisplayName("When 0 Posts")
     @Test
-    void testCalculateWhen0Posts(){
+    void testCalculateWhen0Posts() {
         //given
-        int postsAmmount = 0;
-        int commentsAmmount = 0;
-        List <String> usersAmmount = new ArrayList<>();
+        int postsCount = 0;
+        int commentsCount = 0;
+        List<String> usersCount = new ArrayList<>();
 
-        for (int i=0; i<100; i++){
-            usersAmmount.add("user"+i);
+        for (int i = 0; i < 100; i++) {
+            usersCount.add("user" + i);
         }
 
-        when(statisticsMock.postsCount()).thenReturn(postsAmmount);
-        when(statisticsMock.commentsCount()).thenReturn(commentsAmmount);
-        when(statisticsMock.usersNames()).thenReturn(usersAmmount);
+        when(statisticsMock.postsCount()).thenReturn((double) postsCount);
+        when(statisticsMock.commentsCount()).thenReturn((double) commentsCount);
+        when(statisticsMock.usersNames()).thenReturn(usersCount);
         CountStatistics countStatistics = new CountStatistics(statisticsMock);
         //when
         countStatistics.calculateAdvStatistics();
 
         //then
-        Assertions.assertEquals(100, countStatistics.usersQuantity);
-        Assertions.assertEquals(0, countStatistics.postsQuantity);
-        Assertions.assertEquals(0, countStatistics.commentsQuantity);
-        Assertions.assertEquals(0, countStatistics.averagePostsPerUser);
-        Assertions.assertEquals(0, countStatistics.averageCommentsPerUser);
-        Assertions.assertEquals(0, countStatistics.averageCommentsPerPost);
+        Assertions.assertEquals(100, countStatistics.usersQuantity, 0.01);
+        Assertions.assertEquals(0, countStatistics.postsQuantity, 0.01);
+        Assertions.assertEquals(0, countStatistics.commentsQuantity, 0.01);
+        Assertions.assertEquals(0, countStatistics.averagePostsPerUser, 0.01);
+        Assertions.assertEquals(0, countStatistics.averageCommentsPerUser, 0.01);
+        Assertions.assertEquals(0, countStatistics.averageCommentsPerPost, 0.01);
 
     }
 
     @DisplayName("When 1000 Posts")
     @Test
-    void testCalculateWhen1000Posts(){
+    void testCalculateWhen1000Posts() {
         //given
-        int postsAmmount = 1000;
-        int commentsAmmount = 3000;
-        List <String> usersAmmount = new ArrayList<>();
-        for (int i=0; i<250; i++){
-            usersAmmount.add("user"+i);
+        int postsCount = 1000;
+        int commentsCount = 3000;
+        List<String> usersCount = new ArrayList<>();
+        for (int i = 0; i < 250; i++) {
+            usersCount.add("user" + i);
         }
 
-        when(statisticsMock.usersNames()).thenReturn(usersAmmount);
-        when(statisticsMock.postsCount()).thenReturn(postsAmmount);
-        when(statisticsMock.commentsCount()).thenReturn(commentsAmmount);
+        when(statisticsMock.usersNames()).thenReturn(usersCount);
+        when(statisticsMock.postsCount()).thenReturn((double) postsCount);
+        when(statisticsMock.commentsCount()).thenReturn((double) commentsCount);
         CountStatistics countStatistics = new CountStatistics(statisticsMock);
         //when
         countStatistics.calculateAdvStatistics();
         //then
-        Assertions.assertEquals(250, countStatistics.usersQuantity);
-        Assertions.assertEquals(1000, countStatistics.postsQuantity);
-        Assertions.assertEquals(3000, countStatistics.commentsQuantity);
-        Assertions.assertEquals(4, countStatistics.averagePostsPerUser);
-        Assertions.assertEquals(12, countStatistics.averageCommentsPerUser);
-        Assertions.assertEquals(3, countStatistics.averageCommentsPerPost);
+        Assertions.assertEquals(250, countStatistics.usersQuantity, 0.01);
+        Assertions.assertEquals(1000, countStatistics.postsQuantity, 0.01);
+        Assertions.assertEquals(3000, countStatistics.commentsQuantity, 0.01);
+        Assertions.assertEquals(4, countStatistics.averagePostsPerUser, 0.01);
+        Assertions.assertEquals(12, countStatistics.averageCommentsPerUser, 0.01);
+        Assertions.assertEquals(3, countStatistics.averageCommentsPerPost, 0.01);
     }
 
     @DisplayName("When 0 comments")
     @Test
-    void testCalculateWhen0Comments(){
+    void testCalculateWhen0Comments() {
         //given
-        int postsAmmount = 1000;
-        int commentsAmmount = 0;
-        List <String> usersAmmount = new ArrayList<>();
-        for (int i=0; i<250; i++){
-            usersAmmount.add("user"+i);
+        int postsCount = 1000;
+        int commentsCount = 0;
+        List<String> usersCount = new ArrayList<>();
+        for (int i = 0; i < 250; i++) {
+            usersCount.add("user" + i);
         }
 
-        when(statisticsMock.usersNames()).thenReturn(usersAmmount);
-        when(statisticsMock.postsCount()).thenReturn(postsAmmount);
-        when(statisticsMock.commentsCount()).thenReturn(commentsAmmount);
+        when(statisticsMock.usersNames()).thenReturn(usersCount);
+        when(statisticsMock.postsCount()).thenReturn((double) postsCount);
+        when(statisticsMock.commentsCount()).thenReturn((double) commentsCount);
         CountStatistics countStatistics = new CountStatistics(statisticsMock);
         //when
         countStatistics.calculateAdvStatistics();
         //then
-        Assertions.assertEquals(250, countStatistics.usersQuantity);
-        Assertions.assertEquals(1000, countStatistics.postsQuantity);
-        Assertions.assertEquals(0, countStatistics.commentsQuantity);
-        Assertions.assertEquals(4, countStatistics.averagePostsPerUser);
-        Assertions.assertEquals(0, countStatistics.averageCommentsPerUser);
-        Assertions.assertEquals(0, countStatistics.averageCommentsPerPost);
+        Assertions.assertEquals(250, countStatistics.usersQuantity, 0.01);
+        Assertions.assertEquals(1000, countStatistics.postsQuantity, 0.01);
+        Assertions.assertEquals(0, countStatistics.commentsQuantity, 0.01);
+        Assertions.assertEquals(4, countStatistics.averagePostsPerUser, 0.01);
+        Assertions.assertEquals(0, countStatistics.averageCommentsPerUser, 0.01);
+        Assertions.assertEquals(0, countStatistics.averageCommentsPerPost, 0.01);
     }
 
     @DisplayName("When more posts than comments")
     @Test
-    void testCalculateWhenMorePosts(){
+    void testCalculateWhenMorePosts() {
         //given
-        int postsAmmount = 6000;
-        int commentsAmmount = 3000;
-        List <String> usersAmmount = new ArrayList<>();
-        for (int i=0; i<250; i++){
-            usersAmmount.add("user"+i);
+        int postsCount = 6000;
+        int commentsCount = 3000;
+        List<String> usersCount = new ArrayList<>();
+        for (int i = 0; i < 250; i++) {
+            usersCount.add("user" + i);
         }
 
-        when(statisticsMock.usersNames()).thenReturn(usersAmmount);
-        when(statisticsMock.postsCount()).thenReturn(postsAmmount);
-        when(statisticsMock.commentsCount()).thenReturn(commentsAmmount);
+        when(statisticsMock.usersNames()).thenReturn(usersCount);
+        when(statisticsMock.postsCount()).thenReturn((double) postsCount);
+        when(statisticsMock.commentsCount()).thenReturn((double) commentsCount);
         CountStatistics countStatistics = new CountStatistics(statisticsMock);
         //when
         countStatistics.calculateAdvStatistics();
         //then
-        Assertions.assertEquals(250, countStatistics.usersQuantity);
-        Assertions.assertEquals(6000, countStatistics.postsQuantity);
-        Assertions.assertEquals(3000, countStatistics.commentsQuantity);
-        Assertions.assertEquals(24, countStatistics.averagePostsPerUser);
-        Assertions.assertEquals(12, countStatistics.averageCommentsPerUser);
-        Assertions.assertEquals(0.5, countStatistics.averageCommentsPerPost);
+        Assertions.assertEquals(250, countStatistics.usersQuantity, 0.01);
+        Assertions.assertEquals(6000, countStatistics.postsQuantity, 0.01);
+        Assertions.assertEquals(3000, countStatistics.commentsQuantity, 0.01);
+        Assertions.assertEquals(24, countStatistics.averagePostsPerUser, 0.01);
+        Assertions.assertEquals(12, countStatistics.averageCommentsPerUser, 0.01);
+        Assertions.assertEquals(0.5, countStatistics.averageCommentsPerPost, 0.01);
     }
 
     @DisplayName("When more comments than posts")
     @Test
-    void testCalculateWhenMoreComments(){
+    void testCalculateWhenMoreComments() {
         //given
-        int postsAmmount = 1000;
-        int commentsAmmount = 1500;
-        List <String> usersAmmount = new ArrayList<>();
-        for (int i=0; i<250; i++){
-            usersAmmount.add("user"+i);
+        int postsCount = 1000;
+        int commentsCount = 1500;
+        List<String> usersCount = new ArrayList<>();
+        for (int i = 0; i < 250; i++) {
+            usersCount.add("user" + i);
         }
 
-        when(statisticsMock.usersNames()).thenReturn(usersAmmount);
-        when(statisticsMock.postsCount()).thenReturn(postsAmmount);
-        when(statisticsMock.commentsCount()).thenReturn(commentsAmmount);
+        when(statisticsMock.usersNames()).thenReturn(usersCount);
+        when(statisticsMock.postsCount()).thenReturn((double) postsCount);
+        when(statisticsMock.commentsCount()).thenReturn((double) commentsCount);
         CountStatistics countStatistics = new CountStatistics(statisticsMock);
         //when
         countStatistics.calculateAdvStatistics();
         //then
-        Assertions.assertEquals(250, countStatistics.usersQuantity);
-        Assertions.assertEquals(1000, countStatistics.postsQuantity);
-        Assertions.assertEquals(1500, countStatistics.commentsQuantity);
-        Assertions.assertEquals(4, countStatistics.averagePostsPerUser);
-        Assertions.assertEquals(6, countStatistics.averageCommentsPerUser);
-        Assertions.assertEquals(1.5, countStatistics.averageCommentsPerPost);
+        Assertions.assertEquals(250, countStatistics.usersQuantity, 0.01);
+        Assertions.assertEquals(1000, countStatistics.postsQuantity, 0.01);
+        Assertions.assertEquals(1500, countStatistics.commentsQuantity, 0.01);
+        Assertions.assertEquals(4, countStatistics.averagePostsPerUser, 0.01);
+        Assertions.assertEquals(6, countStatistics.averageCommentsPerUser, 0.01);
+        Assertions.assertEquals(1.5, countStatistics.averageCommentsPerPost, 0.01);
     }
 
     @DisplayName("When 0 users")
     @Test
-    void testCalculateWhen0Users(){
+    void testCalculateWhen0Users() {
         //given
-        int postsAmmount = 10;
-        int commentsAmmount = 20;
-        List <String> usersAmmount = new ArrayList<>();
-        for (int i=0; i<100; i++){
-            usersAmmount.add("user"+i);
+        int postsCount = 150;
+        int commentsCount = 150;
+        List<String> usersCount = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            usersCount.add("user" + i);
         }
-        when(statisticsMock.usersNames()).thenReturn(usersAmmount);
-        when(statisticsMock.postsCount()).thenReturn(postsAmmount);
-        when(statisticsMock.commentsCount()).thenReturn(commentsAmmount);
+        when(statisticsMock.usersNames()).thenReturn(usersCount);
+        when(statisticsMock.postsCount()).thenReturn((double) postsCount);
+        when(statisticsMock.commentsCount()).thenReturn((double) commentsCount);
         CountStatistics countStatistics = new CountStatistics(statisticsMock);
         //when
         countStatistics.calculateAdvStatistics();
         //then
-        Assertions.assertEquals(100, countStatistics.usersQuantity);
-        Assertions.assertEquals(150, countStatistics.postsQuantity);
-        Assertions.assertEquals(150, countStatistics.commentsQuantity);
-        Assertions.assertEquals(1.5, countStatistics.averagePostsPerUser);
-        Assertions.assertEquals(1.5, countStatistics.averageCommentsPerUser);
-        Assertions.assertEquals(1, countStatistics.averageCommentsPerPost);
+        Assertions.assertEquals(100, countStatistics.usersQuantity, 0.01);
+        Assertions.assertEquals(150, countStatistics.postsQuantity, 0.01);
+        Assertions.assertEquals(150, countStatistics.commentsQuantity, 0.01);
+        Assertions.assertEquals(1.5, countStatistics.averagePostsPerUser, 0.01);
+        Assertions.assertEquals(1.5, countStatistics.averageCommentsPerUser, 0.01);
+        Assertions.assertEquals(1, countStatistics.averageCommentsPerPost, 0.01);
     }
 
     @DisplayName("When 100 users")
     @Test
-    void testCalculateWhen100Users(){
+    void testCalculateWhen100Users() {
         //given
-        int postsAmmount = 10;
-        int commentsAmmount = 20;
-        List <String> usersAmmount = new ArrayList<>();
+        int postsCount = 10;
+        int commentsCount = 20;
+        List<String> usersCount = new ArrayList<>();
 
 
-        when(statisticsMock.usersNames()).thenReturn(usersAmmount);
-        when(statisticsMock.postsCount()).thenReturn(postsAmmount);
-        when(statisticsMock.commentsCount()).thenReturn(commentsAmmount);
+        when(statisticsMock.usersNames()).thenReturn(usersCount);
+        when(statisticsMock.postsCount()).thenReturn((double) postsCount);
+        when(statisticsMock.commentsCount()).thenReturn((double) commentsCount);
         CountStatistics countStatistics = new CountStatistics(statisticsMock);
         //when
         countStatistics.calculateAdvStatistics();
