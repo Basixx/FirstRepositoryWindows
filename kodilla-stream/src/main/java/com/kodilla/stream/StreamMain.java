b7ec1ef1a9caaddd0309652fbb3d6e0af5cde90d
Barbara Rutkowska <barbara_rutkowska.22@wp.pl>
2021-10-07 22:50:23 +0200

    7.1 PoemBeautifier

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,45 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.Locale;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+
+        Processor processor = new Processor();
+        Executor codeToExecute = () -> System.out.println("Example text");
+        processor.execute(codeToExecute);
+        processor.execute(() -> System.out.println("example 2"));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Ala ma kota", poem -> "ABC " + poem + " ABC");
+        poemBeautifier.beautify("jestes czarodziejem Harry", poem -> poem.toUpperCase(Locale.ROOT));
+        poemBeautifier.beautify("Nice poem", poem -> poem.replace("Nice", "Beautiful"));
+        poemBeautifier.beautify("    halo akwarium tu ryba    ", poem -> poem.trim());
+        poemBeautifier.beautify("Hello there", poem -> poem.replaceAll(poem, "General Kenobi"));
+
+
+    }
+}
