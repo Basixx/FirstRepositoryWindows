64b35e053c71efd73f4e236787a087ac86f553ca
Barbara Rutkowska <barbara_rutkowska.22@wp.pl>
2021-10-07 23:31:18 +0200

    7.2 ForumUser Class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -40,6 +41,8 @@ public class StreamMain {
         poemBeautifier.beautify("    halo akwarium tu ryba    ", poem -> poem.trim());
         poemBeautifier.beautify("Hello there", poem -> poem.replaceAll(poem, "General Kenobi"));
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
