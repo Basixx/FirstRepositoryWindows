f6998ca5ef54be61d4b18ae09e62a808fa0fe8fd
BSX <barbara_rutkowska.22@wp.pl>
2021-11-30 02:46:03 +0100

    14.3 -1

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -0,0 +1,7 @@
+package com.kodilla.spring.portfolio;
+
+public class Board {
+    private TaskList toDoList = new TaskList();
+    private TaskList inProgressList = new TaskList();
+    private TaskList doneList = new TaskList();
+}
